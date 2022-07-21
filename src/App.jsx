c57cd8623c089a2e5a12a26d16c25d41cb22afd9
ellarbi <ellarbi@student.42.fr>
2022-07-21 22:53:45 +0200

    graph on progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,107 @@
 import './App.css';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
 import React, { useEffect, useState } from 'react';
 
 // // function Display(props) {
 // //   console.log(props);
 // // }
+
+function Graph() {
+  const [array, setArray] = useState([]);
+  const code = "10615120"
+  const url = 'https://hubeau.eaufrance.fr/api/v1/temperature/chronique?code_station=' + code + '&size=1';
+
+  useEffect(() => {
+    const getAllList = async () => {
+      const response = await fetch(url);
+      const jsonData = await response.json();
+      console.log(jsonData);
+      setArray(jsonData);
+      // console.log(array);
+    }
+    getAllList();
+  }, []);
+
+  console.log('oeoelegraph');
+  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100
+    }
+  ];
+  return (
+    <LineChart
+      width={800}
+      height={500}
+      data={data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line
+        type="monotone"
+        dataKey="pv"
+        stroke="#8284d8"
+        activeDot={{ r: 8 }}
+      />
+      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+    </LineChart>
+  );
+}
 
 
 function App() {
@@ -45,17 +143,24 @@ function App() {
   // };
 
   // let results = [];
-  const getAllList = async () => {
-    const response = await fetch(url);
-    const jsonData = await response.json();
-    console.log(jsonData);
-    setArray(jsonData);
-    console.log(array);
-  }
+
+  
 
   useEffect(() => {
+    const getAllList = async () => {
+      const response = await fetch(url);
+      const jsonData = await response.json();
+      console.log(jsonData);
+      setArray(jsonData);
+      // console.log(array);
+    }
     getAllList();
   }, []);
+
+  const callGraph = () => {
+    console.log('callGraph');
+    <Graph />
+  }
 
 
   return (
@@ -69,13 +174,13 @@ function App() {
           <div className="Stations">
             {array.data ? "" : <h1>Fetching data...</h1>}
             {array.data?.map((data, idx) => (
-            <h3 key={idx} className="Station">{data.libelle_station}</h3>
+            <h3 key={idx} className="Station" onClick={() => callGraph()}>{data.libelle_station}, {data.libelle_region}</h3>
             ))}
           </div>
         </div>
         <div className="Graph">
-          <h1>Graphique</h1>
-        </div>
+          <Graph />
+        </div> 
       </div>
     </div>
   );
